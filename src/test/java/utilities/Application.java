777a52314855fde81fde349f1b588d3dd63cd24d
siarhei.yanushkevich <yanushkevichsg@gmail.com>
2023-09-26 01:07:47 +0300

    add singleton

--- src/test/java/utilities/Application.java
+++ src/test/java/utilities/Application.java
@@ -2,28 +2,36 @@ package utilities;
 
 import constants.Timeouts;
 import io.appium.java_client.AppiumDriver;
+
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
     private static AppiumDriver appiumDriver;
 
-    public Application(AppiumDriver appiumDriver) {
-        this.appiumDriver = appiumDriver;
-        this.setImplicitlyWaitToDriver(Timeouts.getImplicitTimeout());
+    public Application() { }
+
+    private static void setImplicitlyWaitToDriver(Duration duration) {
+        getDriver().manage().timeouts().implicitlyWait(duration.getSeconds(), TimeUnit.SECONDS);
     }
 
-    private void setImplicitlyWaitToDriver(Duration duration) {
-        this.getDriver().manage().timeouts().implicitlyWait(duration.getSeconds(), TimeUnit.SECONDS);
+    private static AppiumDriver createDriver() {
+        DriverSettings driverSettings = new DriverSettings();
+        appiumDriver = new AppiumDriver(driverSettings.getConnectionURL(), driverSettings.getCapabilities());
+        setImplicitlyWaitToDriver(Timeouts.getImplicitTimeout());
+        return appiumDriver;
     }
 
     public static AppiumDriver getDriver() {
+        if(appiumDriver == null) {
+            appiumDriver = createDriver();
+        }
         return appiumDriver;
     }
 
     public void quit() {
-        if (this.getDriver() != null) {
-            this.getDriver().quit();
+        if (getDriver() != null) {
+            getDriver().quit();
         }
     }
 }
